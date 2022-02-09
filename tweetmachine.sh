a4e9be02bb4347e7878e66735ddc2ea8a34cf636
P4ul Jeremi4h <72292872+0xcyberpj@users.noreply.github.com>
2022-02-09 15:28:11 +0530

    Update tweetmachine.sh

--- tweetmachine.sh
+++ tweetmachine.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #Author : Dumb Script Writer Cyberpj ! 
 #Basically I dont Know bash Scripting I know only how to create oneliners, I'll Update this script As soon as Possible
-`sudo apt install figlet lolcat -y 2>/dev/null 2>&1`
-echo -e "[+] Insalled Figlet and Lolcat"
+#`sudo apt install figlet lolcat -y 2>/dev/null 2>&1`
+#echo -e "[+] Insalled Figlet and Lolcat"
 chmod +x .
 clear
 print_help() {
@@ -30,7 +30,7 @@ directory=$2
 echo "Current Username : $username"
 echo "Result Directory :" $directory
 output="$username.txt"
-yes=`curl "http://web.archive.org/web/timemap/?url=https%3A%2F%2Ftwitter.com%2F$username%2F&matchType=prefix&collapse=urlkey&output=json&fl=original%2Cmimetype%2Ctimestamp%2Cendtimestamp%2Cgroupcount%2Cuniqcount&filter=!statuscode%3A[45]..&limit=100000&_=1627821432372" 2>/dev/null|grep twitter |cut -d '"' -f2 |tee $directory/$output`
+yes=`curl "http://web.archive.org/web/timemap/?url=https%3A%2F%2Ftwitter.com/$username&matchType=prefix&collapse=urlkey&output=json&fl=original,mimetype" 2>/dev/null|grep twitter |cut -d '"' -f2 |tee $directory/$output`
 `mkdir /tmp/temp/ 2>/dev/null`
 
 echo -ne '#####                     (33%)\r'
