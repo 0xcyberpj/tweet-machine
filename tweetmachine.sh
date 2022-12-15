9247bb88bc0e8ff51252b9cc4ed955b51a50481c
r4vanan <vinothlawrence66@gmail.com>
2022-12-15 23:01:53 +0530

    added some changes

--- tweetmachine.sh
+++ tweetmachine.sh
@@ -26,25 +26,17 @@ echo -e "Tweet-Machine - 0xcyberpj\n"|lolcat 2>/dev/null
 echo -e "\e[36mThis Simple Script Can be Used to Dump All the tweets and replies of the Given user [Target]"
 echo " "
 username=$1
+[[ -d $2 ]] && echo "no need to create!!" || mkdir $2
 directory=$2
 echo "Current Username : $username"
 echo "Result Directory :" $directory
 output="$username.txt"
-yes=`curl "http://web.archive.org/web/timemap/?url=https%3A%2F%2Ftwitter.com/$username&matchType=prefix&collapse=urlkey&output=json&fl=original,mimetype" 2>/dev/null|grep twitter |cut -d '"' -f2 |tee $directory/$output`
+curl -s -# "http://web.archive.org/web/timemap/?url=https%3A%2F%2Ftwitter.com/$username&matchType=prefix&collapse=urlkey&output=json&fl=original,mimetype" 2>/dev/null|grep twitter |cut -d '"' -f2 |tee $directory/$output 2>/dev/null
 `mkdir /tmp/temp/ 2>/dev/null`
 
-echo -ne '#####                     (33%)\r'
-sleep 1
-echo -ne '#############             (66%)\r'
-sleep 1
-echo -ne '####################      (88%)\r'
-sleep 1
-echo -ne '#######################   (100%)\r'
-echo -ne '\n'
-damn=`curl "http://web.archive.org/web/timemap/?url=https://twitter.com/$username&matchType=prefix&collapse=urlkey&output=json&fl=original,mimetype,timestamp" 2>/dev/null >> "/tmp/temp/$username"`
-
-`cat /tmp/temp/$username |cut -d "," -f3 2>/dev/null|cut -d '"' -f2 >> $directory/$output-timeline.txt`
-cat $directory/$output|while read line;do echo "https://web.archive.org/web/0/$line"  >> $directory/$output.webarchive;done
+curl -# "http://web.archive.org/web/timemap/?url=https://twitter.com/$username&matchType=prefix&collapse=urlkey&output=json&fl=original,mimetype,timestamp" 2>/dev/null >> "/tmp/temp/$username"
+echo $(cat /tmp/temp/$username |cut -d "," -f3 2>/dev/null|cut -d '"' -f2) >> $directory/$output-timeline.txt
+echo $(cat $directory/$output|while read line;do echo "https://web.archive.org/web/0/$line";done)  >> $directory/$output.webarchive
 clear
 echo Done!
 echo -e "\e[33mResults saved at $directory/$output"
@@ -54,7 +46,7 @@ head $directory/$output
 echo -n -e "\e[37mFull result Location: $directory/$output"
 echo " "
 echo -n -e "\e[35mThanks For using this Damn Simple Tool"
-sleep 12
+sleep 2
 clear
 exit 0
 #echo "Example http://web.archive.org/web/<timeline>/appropriate-tweetlink"
